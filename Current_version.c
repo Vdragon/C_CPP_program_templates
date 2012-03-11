ea5e025184fcea590092ab0965e7fb6a945a5301
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-11 20:30:21 +0800

    加入檔案

---
+++ Current_version.c
@@ -0,0 +1,35 @@
+﻿/*程式框架(Program Framework)
+-----------------------------------
+程式名稱(program name)：。
+程式描述(program description)：。
+程式版本(program version)：0.00(0)
+程式框架版本(program framework version)：A
+程式框架修訂號(program framework revision number)：201109251624
+更新紀錄(changelog)：
+  Changelog is now stored on github
+已知問題(known issues)：
+  Known issues is now stored on github
+待辦事項(todo)：
+  Todo is now stored on github
+智慧財產授權條款：
+
+*/
+
+/*--------------程式碼開始(Code Started)--------------*/
+/*--------------前期處理器指令(Preprocessor Directive)--------------*/
+/*////////環境設定(Environment Settings)////////*/
+
+/*////////程式所include的標頭檔(Included Headers)////////*/
+
+/*////////常數與巨集(Constants & Macros)以及其他#define指令////////*/
+
+/*////////其他前期處理器指令(Other Preprocessor Directives////////*/
+
+/*--------------全域宣告與定義(Global Declaration & Definition)--------------*/
+/*////////資料結構(Structures)、typedefs跟enumerations////////*/
+
+/*////////函式雛型(Function Prototypes)////////*/
+
+/*////////全域變數(Global Variables)////////*/
+
+/*--------------主要程式碼(Main Code)--------------*/
