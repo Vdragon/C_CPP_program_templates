0a320c1b8ad56571db97bfceb44ceb2ce0f12acb
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-23 06:03:11 +0800

    加入電子郵件地址、加強授權條款適用範圍

--- Current_version.c
+++ Current_version.c
@@ -7,14 +7,15 @@
 待辦事項 | Todos
   Todo is now stored on GitHub
 著作權宣告 | Copyright notice
-  Copyright 2012 林博仁(Henry Lin)
+  Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
 智慧財產授權條款：
-  This program is free software: you can redistribute it and/or modify
+  This program is part of
+  This software is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  This software is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
