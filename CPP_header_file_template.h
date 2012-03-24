6b7ac352dcb3cc228eb905089581ef4ba8e97b32
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-25 02:05:03 +0800

    加入header檔案範本

---
+++ CPP_header_file_template.h
@@ -0,0 +1,7 @@
+﻿/* C++ header檔案範本 */
+/* include guard：避免同一個header檔案被include第二次。*/
+#ifndef _H_INCLUDED
+  #define _H_INCLUDED
+
+
+#endif /* _H_INCLUDED */
---
+++ C_header_file_template.h
@@ -0,0 +1,13 @@
+﻿/* C header檔案範本 */
+/* include guard：避免同一個header檔案被include第二次。*/
+#ifndef _H_INCLUDED
+  #define _H_INCLUDED
+  /* 如果是C++編譯器則停用C++特有的函式名稱mangling*/
+  #ifdef __cplusplus
+    extern "C"{
+  #endif
+
+  #ifdef __cplusplus
+    }
+  #endif
+#endif /* _H_INCLUDED */
