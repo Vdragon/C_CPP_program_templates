e7e9df6afba73914837f5c4e125b2922991ee970
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-02 20:52:08 +0800

    更新header檔案範本：新增定義架構

--- C_header_file_template.h
+++ C_header_file_template.h
@@ -6,6 +6,20 @@
   #ifdef __cplusplus
     extern "C"{
   #endif
+  /*--------------前期處理器指令(Preprocessor Directive)--------------*/
+  /*////////程式所include之函式庫的標頭檔(Included Library Headers)////////*/
+
+  /*////////常數與巨集(Constants & Macros)////////*/
+
+  /*////////其他前期處理器指令(Other Preprocessor Directives////////*/
+
+  /*--------------全域宣告與定義(Global Declaration & Definition)--------------*/
+  /*////////Classes、資料結構(Data Structures)、type definitions跟enumerations////////*/
+
+  /*////////函式和函式雛型(Function & Function Prototypes)////////*/
+
+  /*////////全域變數(Global Variables)////////*/
+
 
   #ifdef __cplusplus
     }
