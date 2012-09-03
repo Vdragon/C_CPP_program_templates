7d4a10834a675c7d3cf3d852968c729cb662d415
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-09-03 15:11:58 +0800

    重新檢視內容，修改如下：
    * space轉tab
    * 新增函式雛型區域
    * 主要內容移動到標頭檔，因為它才是重點

--- C_CPP_source_code_template.c
+++ C_CPP_source_code_template.c
@@ -1,34 +1,15 @@
-﻿/* 程式名稱 | Program name
------------------------------------
-更新紀錄 | Changelog
-  Changelog is now stored on GitHub
-已知問題 | Known Issues
-  Known issues is now stored on GitHub
-待辦事項 | Todos
-  Todo is now stored on GitHub
-著作權宣告 | Copyright notice
-  Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
-智慧財產授權條款：
-  This program is part of this software
-  This software is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This software is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with this software.  If not, see <http://www.gnu.org/licenses/>.
+/* 
+「檔案名稱 File name」
+	請見header檔案以得到更多關於此檔案的說明
 */
 /*||||| 程式所include之函式庫的標頭檔 | Included Library Headers |||||*/
 
-/*|||||常數與巨集 | Constants & Macros |||||*/
+/*||||| 常數與巨集 | Constants & Macros |||||*/
 
 /*||||| Definition of data type, enumeration, data structure and class |||||*/
 
 /*||||| 全域變數 | Global Variables |||||*/
+
+/*||||| 函式雛型 | Function Prototypes |||||*/
 
 /*||||| 主要程式碼 | Main Code |||||*/
--- C_header_file_template.h
+++ C_header_file_template.h
@@ -1,22 +1,41 @@
-﻿/* header名稱 | Header name */
+/* 「檔案名稱 File name」
+程式名稱 | Program name
+	
+更新紀錄 | Changelog
+	Changelog is now stored on GitHub
+已知問題 | Known issues
+	Known issues is now stored on GitHub
+待辦事項 | Todos
+	Todo is now stored on GitHub(http://www.github.com)
+著作權宣告 | Copyright declaration
+	Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
+智慧財產授權條款：
+	This program is part of this software
+	This software is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+	
+	This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License along with this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /* include guard：避免同一個header檔案被include第二次。*/
 #ifndef _H_INCLUDED
-  #define _H_INCLUDED
-  /* 如果是C++編譯器則停用C++特有的函式名稱mangling*/
-  #ifdef __cplusplus
-    extern "C"{
-  #endif
+	#define _H_INCLUDED
+	/* 如果是C++編譯器則停用C++特有的函式名稱mangling*/
+	#ifdef __cplusplus
+		extern "C"{
+	#endif
 
-/*||||| Forward declarations |||||*/
+	/*||||| Forward declarations |||||*/
 
-/*||||| 程式所include之函式庫的標頭檔 | Included Library Headers |||||*/
+	/*||||| 程式所include之函式庫的標頭檔 | Included Library Headers |||||*/
 
-/*||||| 常數與巨集 | Constants & Macros |||||*/
+	/*||||| 常數與巨集 | Constants & Macros |||||*/
 
-/*||||| Definition of data type, enumeration, data structure and class |||||*/
+	/*||||| Definition of data type, enumeration, data structure and class |||||*/
 
-
-  #ifdef __cplusplus
-    }
-  #endif
+	/*||||| 函式雛型 | Function Prototypes |||||*/
+	
+	#ifdef __cplusplus
+		}
+	#endif
 #endif /* _H_INCLUDED */
