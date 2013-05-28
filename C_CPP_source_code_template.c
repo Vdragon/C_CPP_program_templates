0660316958976459623ff0c2902d8bfb36e29727
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-05-29 04:36:47 +0800

    新增 header 範本

--- C_CPP_source_code_template.c
+++ C_CPP_source_code_template.c
@@ -11,7 +11,102 @@
 */
 /* 程式所引入(include)之函式庫的標頭(header)檔案
  * Included library headers */
-
+	/* 標準 C 函式庫
+	 * Standard C libraries
+	 *   C library - C++ Reference
+	 *   http://www.cplusplus.com/reference/clibrary/ */
+		/* C library to perform Input/Output operations
+			#include <stdio.h>
+			#include <cstdio> */
+		/* C Standard General Utilities Library
+			#include <stdlib.h>
+			#include <cstdlib> */
+		/* C error number 
+			#include <errno.h>
+			#include <cerrno> */
+		/* C Diagnostics Library
+			#include <assert.h>
+			#include <cassert> */
+		/* C Character handling functions
+			#include <ctype.h>
+			#include <cctype> */
+		/* Characteristics of floating-point types
+			#include <float.h>
+			#include <cfloat> */		
+		/* ISO 646 Alternative operator spellings
+			#include <iso646.h>
+			#include <ciso646> */
+		/* Sizes of integral types
+			#include <limits.h>
+			#include <climits> */
+		/* C localization library
+			#include <locale.h>
+			#include <clocale> */
+		/* C numerics library
+			#include <math.h>
+			#include <cmath> */
+		/* Non local jumps
+			#include <setjmp.h>
+			#include <csetjmp> */
+		/* C library to handle signals
+			#include <signal.h>
+			#include <csignal> */
+		/* Variable arguments handling
+			#include <stdarg.h>
+			#include <cstdarg> */
+		/* Boolean type(C99 or later)
+			#include <stdbool.h>
+			#include <cstdbool> */
+		/* C Standard definitions
+			#include <stddef.h>
+			#include <cstddef> */
+		/* Integer types(C++11 later)
+			#include <stdint.h>
+			#include <cstdint> */
+		/* C Strings
+			#include <string.h>
+			#include <cstring> */
+		/* C Time Library
+			#include <time.h>
+			#include <ctime> */
+		/* Unicode characters handling library(C++11 later)
+			#include <uchar.h>
+			#include <cuchar> */
+		/* Wide characters handling library
+			#include <wchar.h>
+			#include <cwchar> */
+		/* Wide character type
+			#include <wctype.h>
+			#include <cwctype> */
+		/* Type-generic math(non C89)
+			#include <tgmath.h>
+			#include <ctgmath> */
+		/* 
+			#include <.h>
+			#include <c> */
+		
+	/* 標準 C++ 函式庫 */
+		/* Input/Output related */
+			/* Standard Input / Output Streams Library
+				#include <iostream> */
+			/* Input/output file stream class
+				#include <fstream> */
+		/* Container */
+			/* list
+				#include <list> */
+		/* Unclassified */
+			/* Strings
+				#include <string> */
+			/* Standard Template Library: Algorithms
+				#include <algorithm> */
+			/* 
+				#include <> */
+			
+	/* Ｖ字龍的 C/C++ 函式庫蒐集
+	 * Vdragons C CPP Libraries Collection
+	 *   https://github.com/Vdragon/Vdragons_C_CPP_Libraries_Collection */
+		
+	
 /* 常數與巨集的定義
  * Definition of constants & macros */
 
